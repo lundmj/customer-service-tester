717a11ddfbeff70c33bb90db293d8bfc369752c7
Matthew Lund <lundmatthewj@gmail.com>
2025-12-03 08:29:21 -0700

    Redirect root URL to messages screen

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Include messaging app URLs under /messages
     path("messages/", include("messaging.urls", namespace="messaging")),
+    path("", RedirectView.as_view(url="/messages/", permanent=False)),
 ]
